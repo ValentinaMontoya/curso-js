838195dab649369aade28185c1f25c37def2b73a
ValentinaMontoya <lorenamontoya2004@gmail.com>
2022-07-18 20:26:01 -0500

    clase-1

---
+++ fundamentos/app.js
@@ -0,0 +1,27 @@
+//Imprime un 'Hola Mundo por consola'
+/*
+La siguiente linea
+imprime un 'Hola mundo por consola'
+*/
+console.log("Hola Mundo");
+
+let a = 10,
+  b = 20,
+  c = 30;
+
+  console.log(a)
+  console.log(a)
+  console.log(a)
+
+  console.log('')
+
+  console.log('a', a)
+  console.log('b', b)
+  console.log('c', c)
+
+  console.log('')
+
+  console.log({a})
+  console.log({b})
+  console.log({c})
+
